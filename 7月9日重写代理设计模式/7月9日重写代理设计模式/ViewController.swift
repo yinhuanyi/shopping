4ba5b5beec9d29eebc7b7bff3a22d9d1586c90d8
尹欢一 <yinhuanyi@yinhuanyideair.lan>
2016-07-09 00:55:14 +0800

    初始化项目

---
+++ 7月9日重写代理设计模式/7月9日重写代理设计模式/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  7月9日重写代理设计模式
+//
+//  Created by 尹欢一 on 16/7/9.
+//  Copyright © 2016年 hut. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
